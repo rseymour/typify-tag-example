3a93d95495027fbf5100f99d3967149b75147a4a
Rich Seymour <rich@runsybil.com>
2024-02-01 10:27:36 -0500

    simple example of need for tag

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+// Copyright 2023 Oxide Computer Company
+
+use typify::import_types;
+
+use serde::{Deserialize, Serialize};
+use std::default;
+// works with tag = "my_tag" annotation changed
+//mod simple;
+//use simple::MainModel;
+
+import_types!(
+    schema = "simple.json",
+);
+
+#[test]
+fn test_main() {
+    main()
+}
+
+fn main() {
+    let data = r#"
+        {
+        "fbz":[{
+            "my_type": "Baz",
+            "count": 3
+        }]}"#;
+
+    let mm: MainModel = serde_json::from_str(data).unwrap();
+// comes out as FooBar instead of Baz on my machine
+
+    println!("{:?}", mm);
+}
